94ba816f391f93d2ee45c83ec5278696d4ff0870
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-16 10:39:36 +0200

    Move pdf2md to its own repo

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -14,7 +14,6 @@ import smtplib
 from more_itertools import chunked
 
 from common import change_tracking
-from common.pdf2md import Converter
 from common.retry import retry
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
@@ -540,33 +539,3 @@ def is_file_locked(file_path):
             return False
     except PermissionError:
         return True
-
-
-def convert_pdf_to_md(pdf_url, pdf_path, prefix):
-    # Download the PDF
-    logging.info(f"   Downloading PDF: {pdf_url}")
-    try:
-        r_pdf = requests_get(pdf_url)
-        with open(pdf_path, "wb") as file:
-            for chunk in r_pdf.iter_content(chunk_size=1024):
-                file.write(chunk)
-        print(f"PDF downloaded successfully as {pdf_path}")
-    except:
-        print("Failed to download PDF.")
-        return dict()
-
-    # Convert the PDF to Markdown
-    methods = ["docling", "pymupdf4llm", "pymupdf"]
-    markdowns = {}
-    for m in methods:
-        converter = Converter(lib=m, input_file=pdf_path)
-        try:
-            converter.convert()
-            markdown_path = converter.output_file
-            with open(markdown_path, "r", encoding="utf-8") as f:
-                markdowns[f'{prefix}_{m}'] = f.read()
-        except:
-            logging.error(f"Failed to convert PDF using method: {m}")
-            continue
-
-    return markdowns
